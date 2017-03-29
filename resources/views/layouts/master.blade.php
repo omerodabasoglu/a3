f3a9d0ef2ff472da7ee86264b17a1b84f5206e46
Omer Odabasoglu <omerpasha34@gmail.com>
2017-03-29 14:18:53 -0700

    Updated the favicon.ico

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
 
     <title>@yield('title', 'Assignment 3')</title>
     <meta charset="utf-8">
-    <link rel="icon" href="images/favicon.ico/OO-57x57.png" />
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" >
     <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah%7CUbuntu" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/united/bootstrap.min.css" />
